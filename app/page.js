b2b5b64c94513ca7017c4b9e7b2a505862fbb5f0
Mendozacidulysses-cyber <mendozacidulysses@icloud.com>
2026-03-16 03:56:55 -0600

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,29 @@
+export default function Page() {
+  return (
+    <main style={{
+      fontFamily: "Arial",
+      padding: 40,
+      textAlign: "center"
+    }}>
+      <h1>Barber AI</h1>
+
+      <p>Plataforma inteligente para barberías.</p>
+
+      <p>Proyecto en construcción.</p>
+
+      <div style={{marginTop:40}}>
+        <button style={{
+          padding:"12px 20px",
+          fontSize:16,
+          background:"black",
+          color:"white",
+          borderRadius:8,
+          border:"none"
+        }}>
+          Übermensch Barbería
+        </button>
+      </div>
+
+    </main>
+  )
+}
